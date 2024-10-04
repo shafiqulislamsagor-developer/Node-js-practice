d280cbcae23e1cffce5f9e6e52d95f9a928e2a44
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-04 23:46:44 +0600

    build in module learn

---
+++ build-In module/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+// console.log(path.dirname("/D:/Project/learning-node-js/index.js"));
+
+// console.log(path.parse("/D:/Project/learning-node-js/index.js"));
+
+console.log(path.join("/D:/Project/learning-node-js/", "local1.js"));
